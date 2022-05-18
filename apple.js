1ea21a38ff3a644f0d98a9acfe741e0f6ad724bb
Slettum Boston <816661@stma.k12.mn.us>
2022-05-18 12:21:43 -0500

    new stma finished

--- apple.js
+++ apple.js
@@ -1,1 +1,33 @@
-// JavaScript Document
+function stma1(){
+	window.open("https://stmaknights.com/");	
+}
+function stma2(){
+	window.open("https://www.stma.k12.mn.us/Page/2#calendar1/20220425/month");
+}
+function stma3(){
+	window.open("https://www.stma.k12.mn.us/domain/4555");
+}
+function stma4(){
+	window.open("https://pvue7.region1.k12.mn.us/rt0885/PXP2_Login.aspx");
+}
+function stma5(){
+	window.open("https://stma.nutrislice.com/menus-eula");
+}
+function stma6(){
+	window.open("https://www.stma.k12.mn.us/domain/2433");
+}
+function stma7(){
+	window.open("https://pac.stma.k12.mn.us/scripts/wsisa.dll/WService=wsFin/seplog01.w");
+}
+function stma8(){
+	window.open("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+}
+
+document.getElementById("p1").addEventListener("click", stma1);
+document.getElementById("p2").addEventListener("click", stma2);
+document.getElementById("p3").addEventListener("click", stma3);
+document.getElementById("p4").addEventListener("click", stma4);
+document.getElementById("p5").addEventListener("click", stma5);
+document.getElementById("p6").addEventListener("click", stma6);
+document.getElementById("p7").addEventListener("click", stma7);
+document.getElementById("p8").addEventListener("click", stma8);
